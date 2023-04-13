8e2e09e4a14a6b4448ce57326343d351b0cb8365
Yuri <yuriqpaiva@gmail.com>
2023-04-12 22:39:32 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/defaultTheme'
+import { Home } from './pages/Home'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Home />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
